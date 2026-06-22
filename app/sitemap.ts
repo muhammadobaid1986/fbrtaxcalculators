48a14980f4cd0435e5b4c93a78951ec1ab2a3eca
muhammadobaid1986 <muhammadobai786@gmail.com>
2026-06-22 11:15:45 +0500

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,5 +24,61 @@ export default function sitemap() {
       url: "https://fbrtaxcalculators.vercel.app/income-tax-calculator-2021-22",
       lastModified: new Date(),
     },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/zakat-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/withholding-tax-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/disclaimer",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/filer-vs-non-filer-2025",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/how-to-become-a-filer",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/how-to-file-tax-return-online-pakistan",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/property-tax-in-pakistan",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/tax-on-bank-profit",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/tax-on-car-registration",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fbrtaxcalculators.vercel.app/latest-budget-2025-26-tax-slabs",
+      lastModified: new Date(),
+    },
   ];
 }
